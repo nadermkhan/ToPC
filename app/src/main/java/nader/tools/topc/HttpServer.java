6244f50ad8e628f900a2f29e9a600eb5bce027fd
Nader Mahbub Khan <muhammadnadermahbubkhan@gmail.com>
2024-08-11 01:51:38 -0400

    try to fix

--- app/src/main/java/nader/tools/topc/HttpServer.java
+++ app/src/main/java/nader/tools/topc/HttpServer.java
@@ -3,18 +3,25 @@ package nader.tools.topc;
 import android.os.Environment;
 import android.util.Log;
 import androidx.annotation.NonNull;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.IOUtils;
 import fi.iki.elonen.NanoHTTPD;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 import java.util.Map;
 
 public class HttpServer extends NanoHTTPD {
     private static final String TAG = "HttpServer";
+    private static final FileItemFactory fileItemFactory = new DiskFileItemFactory();
     private OnStatusUpdateListener mStatusUpdateListener;
 
     public interface OnStatusUpdateListener {
@@ -56,35 +63,35 @@ public class HttpServer extends NanoHTTPD {
         Log.d(TAG, "header=" + header);
         Log.d(TAG, "params=" + parms);
 
-        if (Method.POST.equals(method) && session.getHeaders().containsKey("content-type") && 
-            session.getHeaders().get("content-type").startsWith("multipart/form-data")) {
-            // Handle file upload
+        if (Method.POST.equals(method) && ServletFileUpload.isMultipartContent(session)) {
             try {
-                // Custom file upload handling for NanoHTTPD
-                Map<String, String> files = session.getFiles();
-                for (Map.Entry<String, String> entry : files.entrySet()) {
-                    String name = entry.getKey();
-                    String fileName = entry.getValue();
-                    
-                    File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
-                    Log.d(TAG, "Save file to " + file.getAbsolutePath());
-                    if (mStatusUpdateListener != null) {
-                        mStatusUpdateListener.onUploadingFile(file, false);
-                    }
+                ServletFileUpload upload = new ServletFileUpload(fileItemFactory);
+                List<FileItem> items = upload.parseRequest(session);
+                for (FileItem item : items) {
+                    String name = item.getFieldName();
+                    if (item.isFormField()) {
+                        Log.d(TAG, "Item is form field, name=" + name + ", value=" + item.getString());
+                    } else {
+                        String fileName = item.getName();
+                        Log.d(TAG, "Item is file field, name=" + name + ", fileName=" + fileName);
 
-                    try (InputStream inputStream = session.getInputStream();
-                         FileOutputStream fos = new FileOutputStream(file)) {
-                        byte[] buffer = new byte[1024];
-                        int bytesRead;
-                        while ((bytesRead = inputStream.read(buffer)) != -1) {
-                            fos.write(buffer, 0, bytesRead);
+                        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
+                        String path = file.getAbsolutePath();
+                        Log.d(TAG, "Save file to " + path);
+                        if (mStatusUpdateListener != null) {
+                            mStatusUpdateListener.onUploadingFile(file, false);
+                        }
+
+                        try (InputStream inputStream = item.getInputStream();
+                             FileOutputStream fos = new FileOutputStream(file)) {
+                            IOUtils.copy(inputStream, fos);
+                        }
+                        if (mStatusUpdateListener != null) {
+                            mStatusUpdateListener.onUploadingFile(file, true);
                         }
                     }
-                    if (mStatusUpdateListener != null) {
-                        mStatusUpdateListener.onUploadingFile(file, true);
-                    }
                 }
-            } catch (IOException e) {
+            } catch (FileUploadException | IOException e) {
                 e.printStackTrace();
                 return newFixedLengthResponse("Error uploading file!");
             }
